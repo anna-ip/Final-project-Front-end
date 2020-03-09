fba1515d966d6ea971a4fb0db189fb34b142c9a0
sarah wolfson <swolfson23@gmail.com>
2020-03-09 13:43:59 +0100

    redux

--- App.js
+++ App.js
@@ -1,41 +1,50 @@
-import React from "react"
-import { StyleSheet, Text, SafeAreaView, ScrollView, View } from 'react-native';
-import Constants from 'expo-constants';
-import RecepieCard from './Components/RecepieCard'
-import styled from 'styled-components/native'
+import React from "react";
+import { StyleSheet, Text, SafeAreaView, ScrollView, View } from "react-native";
+import Constants from "expo-constants";
+import RecepieCard from "./Components/RecepieCard";
+import styled from "styled-components/native";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { food } from "./Reducer/food.js";
 
+const reducer = combineReducers({
+  food: food.reducer
+});
+
+const store = configureStore({ reducer });
 
 const App = () => (
-  <StyledView>
-    <StyledSafeAreaView>
-      <Scroll horizontal={true}>
-        <RecepieCard />
-        <RecepieCard />
-        <RecepieCard />
-        <RecepieCard />
-      </Scroll>
-    </StyledSafeAreaView>
-  </StyledView>
-)
-export default App
+  <Provider store={store}>
+    <StyledView>
+      <StyledSafeAreaView>
+        <Scroll horizontal={true}>
+          <RecepieCard />
+          <RecepieCard />
+          <RecepieCard />
+          <RecepieCard />
+        </Scroll>
+      </StyledSafeAreaView>
+    </StyledView>
+  </Provider>
+);
+export default App;
 
 const StyledView = styled.View`
- display: flex;
- flex-direction: column;
- justify-content: center;
- align-content: center;
- margin-top: 100px;
-`
-const StyledSafeAreaView = styled(SafeAreaView)`
- display: flex;
- flex-direction: column;
- justify-content: center;
- align-content: center;
-`
-const Scroll = styled(ScrollView)`
- background-color: #FFFFFF;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  margin-top: 100px;
 `;
-
+const StyledSafeAreaView = styled(SafeAreaView)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+`;
+const Scroll = styled(ScrollView)`
+  background-color: #ffffff;
+`;
 
 /*
 const App = () => {
