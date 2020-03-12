929c6f8e3526a0c1123b70d46fa497471a1e1b87
sarah wolfson <swolfson23@gmail.com>
2020-03-12 15:50:20 +0100

    styling of VeggieIcons on searchPage

--- Components/SearchPage.js
+++ Components/SearchPage.js
@@ -2,41 +2,32 @@ import React from "react";
 import { NativeRouter, Route, Link } from "react-router-native";
 import VeggieIcons from "./lib/VeggieIcons";
 import SearchButton from "./lib/SearchButton";
-import { View, Button } from 'react-native';
-
-import styled from 'styled-components/native';
+import { View, Button } from "react-native";
+import styled from "styled-components/native";
+import { Line } from "react-native-svg";
 
 const SearchPage = ({ history }) => {
-    return (
-        <View>
+  return (
+    <View>
+      <Title>Welcome</Title>
+      <View
+        style={{
+          borderBottomColor: "black",
+          borderBottomWidth: 1,
+          marginLeft: 5,
+          marginRight: 5
+        }}
+      />
+      <VeggieIcons />
 
-            {/* <VeggieIcons /> */}
-            {/* <SearchButton /> */}
-            {/* <Link
-                to={`/recepieIndex`}> */}
-            <Title>Welcome</Title>
-
-
-            {/* <Button id={props.id} title='Cucumber' onPress={() => 1} />
-            <Button id={props.id} title='EggPlant' onPress={() => 2} />
-            <Button id={props.id} title='Tomato' onPress={() => 3} />
-            <Button id={props.id} title='Onion' onPress={() => 4} />
-            <Button id={props.id} title='Carrot' onPress={() => 5} />
-            <Button id={props.id} title='Potato' onPress={() => 6} />
-            <Button id={props.id} title='Fennel' onPress={() => 7} /> */}
-
-
-            <Button title='Search' onPress={() => history.push('/RecepieIndex')} />
-            {/* <VeggieIcons />*/}
-            {/* <SearchButton /> */}
-            {/* </Link> */}
-        </View>
-
-    )
-
-}
+      <Button title="Search" onPress={() => history.push("/RecepieIndex")} />
+    </View>
+  );
+};
 
 const Title = styled.Text`
-color: black;
-`
+  color: black;
+  text-align: center;
+`;
+
 export default SearchPage;
--- Components/lib/VeggieIcons.js
+++ Components/lib/VeggieIcons.js
@@ -1,37 +1,91 @@
-import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import * as React from "react";
+import {
+  View,
+  Image,
+  ScrollView,
+  TouchableHighlight,
+  TouchableOpacity,
+  SafeAreaView
+} from "react-native";
 
+import styled from "styled-components";
 
-export default class VeggieIcons extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <BeetIcon />
-        <Image
+const veggieIcons = () => {
+  return (
+    <Container>
+      {/* ScrollView row 1 */}
+      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+        <IconContainer>
+          {/* Asparagus */}
+          <Button></Button>
+          {/* Beet */}
+          <Button></Button>
+          {/* Broccoli */}
+          <Button></Button>
+          {/* Cabage */}
+          <Button></Button>
+          {/* Cauliflower */}
+          <Button></Button>
+          {/* Spininch */}
+          <Button></Button>
+        </IconContainer>
+      </ScrollView>
+      {/* ScrollView row 2 */}
+      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+        <IconContainer>
+          {/* Cucumber */}
+          <Button></Button>
+          {/* Eggplant */}
+          <Button></Button>
+          {/* Mushroom */}
+          <Button></Button>
+          {/* Pepper */}
+          <Button></Button>
+          {/* Pumpkin */}
+          <Button></Button>
+          {/* Tomato */}
+          <Button></Button>
+        </IconContainer>
+      </ScrollView>
+    </Container>
+  );
+};
 
-        />
-        <TouchableOpacity style={styles.button}></TouchableOpacity>
+export default veggieIcons;
 
-      </View>
-    );
-  }
-}
+// const Icon = styled.Image`
+//   width: 60px;
+//   height: 60px;
+//   padding: 20px;
+// `
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  button: {
-    backgroundColor: "#859a9b",
-    borderRadius: 20,
-    padding: 10,
-    marginBottom: 20,
-    shadowColor: "#303838",
-    shadowOffset: { width: 0, height: 5 },
-    shadowRadius: 10,
-    shadowOpacity: 0.35
-  }
-});
+const Container = styled.SafeAreaView`
+  height: 300px;
+  margin: 20px;
+`;
+
+const IconContainer = styled.SafeAreaView`
+  flex-direction: row;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  height: 40px;
+`;
+
+const Button = styled.TouchableOpacity`
+  border: 3px solid #ffffff;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 90px;
+  width: 90px;
+  border: solid;
+  padding: 10px;
+  shadow-color: #303838;
+  shadow-radius: 10px;
+  border-radius: 60px;
+  shadow-opacity: 0.35px;
+  margin: 5px;
+`;
+
+const ButtonIcons = styled.View``;
